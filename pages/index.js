0ddb4750dad42d249b8e6110fd9cb095afbdfa05
ahah12k <ahah12k@gmail.com>
2020-11-18 07:46:34 +0900

    very little change

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,34 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Mass Data</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="the-body">
-        <ul>
-          <li>
-            <Link href="/paging">
-              <a>Paging</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/scrolling">
-              <a>Scrolling</a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Mass Data</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <div className="the-body">
+                <ul>
+                    <li>
+                        <Link href="/paging">
+                            <a>Paging</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/scrolling">
+                            <a>Scrolling</a>
+                        </Link>
+                    </li>
+                  <li>
+                    will be Gikal
+                  </li>
+                  <li>
+                    will be WebGame
+                  </li>
+                </ul>
+            </div>
+        </div>
+    )
 }
